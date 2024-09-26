569a3f2b13acf00c84d6688e50c4096d67096d9c
Tom Waterman <tjwaterman99@gmail.com>
2024-09-26 08:51:37 +0000

    Update some layouts

---
+++ layouts/default.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="container mx-auto max-w-xl bg-gray-100 p-4">
+        <slot />
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,10 @@
 <template>
-<h1>Tom's Blog</h1>
+<h1 class="font-bold text-3xl mb-4">Tom's Blog</h1>
 <ul>
-    <li v-for="page in homepage.results">
-        <NuxtLink :to="{name: 'articles-slug', params: {'slug': page.id}}">{{ page.child_page }}</NuxtLink>
+    <li v-for="page in homepage.results" class="mt-2 flex justify-between">
+        <NuxtLink :to="{name: 'articles-slug', params: {'slug': page.id}}" class="hover:text-blue-400">{{ page.child_page.title }}</NuxtLink>
+        <p>{{ formatDate(page.created_time) }}</p>
+
     </li>
 </ul>
 </template>
@@ -13,7 +15,13 @@ const client = new Client({
     auth: process.env.NOTION_API_KEY
 })
 
+
+function formatDate(ts) {
+    var d = new Date(ts)
+    return d.toDateString()
+}
+
 const homepage = await client.blocks.children.list({
-    block_id: '4dd05d0e0ea6432cbeac0ddeaf4c0e92', page_size: 50
+    block_id: '10d87f475d79800d88f5fc65da2df06b', page_size: 50
 })
 </script>
