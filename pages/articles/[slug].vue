ccd101f3e707c715d05734f15e0ced299db33786
Tom Waterman <tjwaterman99@gmail.com>
2024-09-26 07:57:05 +0000

    First commit

---
+++ pages/articles/[slug].vue
@@ -0,0 +1,17 @@
+<template>
+    <p></p>
+    <p v-for="block of page_details.results">{{ block }}</p>
+</template>
+
+<script setup>
+const route = useRoute();
+const block_id = route.params.slug;
+import { Client } from '@notionhq/client';
+const client = new Client({
+    auth: process.env.NOTION_API_KEY
+})
+
+const page_details = await client.blocks.children.list({
+    block_id: block_id, page_size: 50
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+<h1>Tom's Blog</h1>
+<ul>
+    <li v-for="page in homepage.results">
+        <NuxtLink :to="{name: 'articles-slug', params: {'slug': page.id}}">{{ page.child_page }}</NuxtLink>
+    </li>
+</ul>
+</template>
+
+<script setup>
+import { Client } from '@notionhq/client';
+const client = new Client({
+    auth: process.env.NOTION_API_KEY
+})
+
+const homepage = await client.blocks.children.list({
+    block_id: '4dd05d0e0ea6432cbeac0ddeaf4c0e92', page_size: 50
+})
+</script>
